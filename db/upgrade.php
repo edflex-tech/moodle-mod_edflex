d4bb7318e1cb839809da9995f896921d4cb960ec
Damien <damien.vauchel@edflex.com>
2026-06-24 12:40:08 +0200

    🐛 EDFLEX1-14964: declare main edflex table to fix course deletion
    Moodle's remove_course_contents() runs SELECT ... FROM mdl_{modname} for
    every installed module on course deletion. The missing main edflex table
    raised a dmlreadexception that broke deletion of any course platform-wide.
    
    - Add the edflex table to db/install.xml
    - Create it on existing installs in db/upgrade.php (guarded by table_exists),
      bump version to 2026062400
    - Make edflex_delete_instance purge its row instead of returning false

--- db/upgrade.php
+++ db/upgrade.php
@@ -30,5 +30,35 @@
  * @return bool
  */
 function xmldb_edflex_upgrade(int $oldversion): bool {
+    global $DB;
+
+    $dbman = $DB->get_manager();
+
+    if ($oldversion < 2026062400) {
+        // Define table edflex to be created.
+        $table = new xmldb_table('edflex');
+
+        // Adding fields to table edflex.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('course', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+        $table->add_field('name', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('intro', XMLDB_TYPE_TEXT, null, null, null, null, null);
+        $table->add_field('introformat', XMLDB_TYPE_INTEGER, '4', null, XMLDB_NOTNULL, null, '0');
+        $table->add_field('timemodified', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+
+        // Adding keys to table edflex.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('fk_course', XMLDB_KEY_FOREIGN, ['course'], 'course', ['id']);
+
+        // Conditionally launch create table for edflex.
+        // A client may have created the table manually as an emergency workaround.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Edflex savepoint reached.
+        upgrade_mod_savepoint(true, 2026062400, 'edflex');
+    }
+
     return true;
 }
