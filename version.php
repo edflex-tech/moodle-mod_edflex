61b6501a4ce347431083f9b403f5819327e5da81
Damien <damien.vauchel@edflex.com>
2026-06-29 16:49:53 +0200

    🔖 Bump plugin version to 2026062901

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_edflex';
 $plugin->release = 'v1.0.2';
-$plugin->version = 2026062900;
+$plugin->version = 2026062901;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
